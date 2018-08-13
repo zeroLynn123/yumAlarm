1dea8468789b5ccab675635f2e98f464b88e39fc
Lynn <lynn@linruipengs-MacBook-Pro.local>
2018-08-13 11:33:33 +0800

    创建云瞳报警服务

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/getAlarmData', function(req, res, next) {
+  console.log(req.body)
+});
+
+module.exports = router;
